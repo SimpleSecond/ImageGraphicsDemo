9dd12f76a05490f0df22a3fe42890bd096c6980a
WangDongya <wangdongya@wangdongyademacbook-pro.local>
2016-08-20 08:45:32 +0800

    Initial Commit

---
+++ ImgGraphicsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImgGraphicsDemo
+//
+//  Created by WangDongya on 16/8/20.
+//  Copyright © 2016年 WangDongya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
